57953deef6115cbc800faf779d6c2b443d3cbc5d
KSand85 <133282819+KSand85@users.noreply.github.com>
2023-05-16 14:49:16 -0400

    Add project files.

---
+++ Empower2Script_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower2Script_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower2Script_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0EAD27EC-F5D9-4616-93DB-0E72E69EBB35")]
+[assembly: AssemblyVersion("1.0.0.0")]
